cd5f10aa339b26dfb9ddfbaf02964e2beb8e6b9d
phantanhungpy20061@gmail.com <phantanhungpy20061@gmail.com>
2026-03-25 23:38:33 +0700

    hw

---
+++ TetPee.Service/Product/Response.cs
@@ -0,0 +1,12 @@
+namespace TetPee.Service.Product;
+
+public class Response
+{
+    public static class Messages
+    {
+        public static string CreateProductSuccess = "Create successfully Product";
+        public static string UpdateProductSuccess = "Update successfully Product";
+        public static string FailedToCreateProduct = "Failed to create product";
+    }
+}
+
--- TetPee.Service/Seller/RequestSeller.cs
+++ TetPee.Service/Seller/RequestSeller.cs
@@ -2,9 +2,8 @@ namespace TetPee.Service.Seller;
 
 public class RequestSeller
 {
-    public class CreateUserRequest
+    public class CreateSellerRequest: Identity.Request.CreateUserRequest
     {
-        
         public string TaxCode { get; set; }
         public string CompanyName { get; set; }
         public string CompanyAddress { get; set; }
