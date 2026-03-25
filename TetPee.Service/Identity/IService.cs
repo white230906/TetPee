a2f89a5167195e35331601c7226c61f57720928e
phantanhungpy20061@gmail.com <phantanhungpy20061@gmail.com>
2026-03-25 19:02:19 +0700

    check

--- TetPee.Service/Identity/IService.cs
+++ TetPee.Service/Identity/IService.cs
@@ -5,7 +5,7 @@ public interface IService
     public Task<Response.IdentityResponse> Login(string email, string password);
 }
 //Nhiệm vụ của Identity:
-    //Kiểm tra email/password
-    //Nếu đúng -> xác nhận user
-    //Tạo token (gọi jwtService)
-    //Trả token về cho client
+//Kiểm tra email/password
+//Nếu đúng -> xác nhận user
+//Tạo token (gọi jwtService)
+//Trả token về cho client
--- TetPee.Service/JwtService/IService.cs
+++ TetPee.Service/JwtService/IService.cs
@@ -7,6 +7,6 @@ public interface IService
     //tạo token đưa về cho client
     public string GenerateAccessToken(IEnumerable<Claim> claims);
     //khi client gọi API
-        //kiểm tra token có hợp lệ không
+    //kiểm tra token có hợp lệ không
     ClaimsPrincipal ValidateToken(string token);
 }
